94700812a734973666a2adfb9b12d6a4def09e4b
ChaeyeonHan <kate2236e@naver.com>
2021-11-06 13:17:16 +0900

    ADD points

--- client/eyesee/app/src/main/java/com/yblee2001/eyesee/ui/user/PointsActivity.kt
+++ client/eyesee/app/src/main/java/com/yblee2001/eyesee/ui/user/PointsActivity.kt
@@ -1,7 +1,11 @@
 package com.yblee2001.eyesee.ui.user
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.yblee2001.eyesee.R
 import com.yblee2001.eyesee.databinding.ActivityPointsBinding
 
 class PointsActivity : AppCompatActivity() {
@@ -12,6 +16,21 @@ class PointsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPointsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var plus = findViewById<Button>(R.id.plus_btn)
+        val output = findViewById<TextView>(R.id.points)
+        val output_text : String = "points";
+        var num = 0;
+
+        plus.setOnClickListener{
+            num++
+            output.setText(num.toString() + output_text)
+        }
+    }
+
+    private fun loadDate(){
+        val pref = this.getPreferences(0) // 저장된 값이 없으면 0가져오기
+
 
     }
 }
